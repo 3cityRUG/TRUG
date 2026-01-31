815738c0da0b50a14d6833a3e1de94d5e469e5e2
Oskar Szrajer <oskarszrajer@gmail.com>
2026-01-31 16:02:40 +0100

    feat: Add Polish pluralization helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Polish pluralization helper
+  # Usage: polish_pluralize(count, "prezentacja", "prezentacje", "prezentacji")
+  def polish_pluralize(count, one, few, many)
+    return many if count == 0
+    case count % 100
+    when 1 then one
+    when 2..4 then few
+    else many
+    end
+  end
 end
